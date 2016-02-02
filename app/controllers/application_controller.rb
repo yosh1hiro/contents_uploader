637fbff0e11418d9d7f5e882a990a2810c2a2292
yoshihiro asada <yoshihiro0921@gmail.com>
2016-02-02 14:36:36 +0900

    Add layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < Sinatra::Base
 
   set :views, File.expand_path('../../views', __FILE__)
+
+  before '/*' do
+    @title = "まごチャンネル"
+  end
   
   configure :production, :development do
     enable :logging
