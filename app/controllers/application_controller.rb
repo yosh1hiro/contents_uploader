1032b37843b869b3be46be7e734b964c5c2cf278
yoshihiro asada <yoshihiro0921@gmail.com>
2016-02-02 10:54:03 +0900

    pre set up

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < Sinatra::Base
+
+  # set :views, File.expand_path('../../views', __FILE__)
+  
+  configure :production, :development do
+    enable :logging
+  end
+
+  get '/' do
+    "Hello, World!"
+  end
+end
